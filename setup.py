3ae77226c00110eeed367f740a6d93eb63dca71e
glebk <gleb.kuznetsov@gmail.com>
2014-04-28 15:49:02 -0400

    Create setup.py.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# NOTE: multiprocessing import required for issues with nose tests.
+# See: http://bugs.python.org/issue15881#msg170215
+import multiprocessing
+
+from setuptools import setup
+
+setup(
+    name='reference_genome_maker',
+    version='0.1',
+    author='Church Lab',
+    author_email='gleb@mit.edu',
+    maintainer='Gleb Kuznetsov',
+    maintainer_email='gleb@mit.edu',
+    url='http://churchlab.github.io/millstone/',
+    package_dir={'': 'src'},
+    packages=['reference_genome_maker'],
+    install_requires=[
+        'biopython >= 1.6.1'
+        'PyVCF >= 0.6.7'
+    ],
+    test_suite='nose.collector',
+)
